c0e51def83d44b080289debac3a671494d6f5393
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2019-10-20 16:53:27 +0100

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCA901IZMnqeXsCamKxl6JjwaBwD2CywL0",
-  projectId: "instapro-ad710",
-  storageBucket: "instapro-ad710.appspot.com",
-  mongoURI:
-    "mongodb+srv://admin:InstaPro@cluster0-kgh4c.mongodb.net/test?retryWrites=true",
-  ourSecret: "sUPeRsEcReTPasSwOrD"
-};
